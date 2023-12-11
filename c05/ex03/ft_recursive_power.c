1e5ac6710956bdf44df58c6cd0c47310cef888fc
WarfaXDVR <152220767+Warfa777@users.noreply.github.com>
2023-12-11 05:46:18 +0100

    Add files via upload

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliberat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 04:34:29 by sliberat          #+#    #+#             */
+/*   Updated: 2023/12/11 04:54:55 by sliberat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power > 1)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	else
+	{
+		return (nb);
+	}
+}
